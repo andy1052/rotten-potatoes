1d4dd61a98e78d0e409844e722b4a3a2ac3e8d07
andy1052 <andy.ducharme@gmail.com>
2018-10-28 00:06:24 -0600

    Users can now edit and update reviews

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 
 //	Make connection to mongoDB:
@@ -40,6 +41,8 @@ app.set('view engine', 'handlebars');
 
 //	This line MUST appear AFTER app = express(), but BEFORE your routes!:
 app.use(bodyParser.urlencoded({ extended: true }));
+//	This middleware also Must appear After app = express(), but BEFORE your routes!:
+app.use(methodOverride('_method'));
 
 
 //	Make inital route:
@@ -76,10 +79,31 @@ app.post('/reviews', (req, res) => {
 app.get('/reviews/:id', (req, res) => {
 	//	Find specified document by _id:
 	Review.findById(req.params.id).then((review) => {
-		res.render('reviews-show', { review: review})
+		res.render('reviews-show', { review: review })
 	}).catch((err) => {
 		console.log(err.message);
 	});
+});
+
+
+//	Edit review route:
+app.get('/reviews/:id/edit', (req, res) => {
+	Review.findById(req.params.id).then((review) => {
+		res.render('reviews-edit', { review: review })
+	}).catch((err) => {
+		console.log(err.message);
+	});
+});
+
+
+//	Put request (Update), using "method-override" middleware:
+app.put('/reviews/:id', (req, res) => {
+	Review.findOneAndUpdate(req.params.id, req.body)
+		.then(review => {
+			res.redirect(`/reviews/${review._id}`);
+		}).catch(err => {
+			console.log(err.message);
+		});
 });
 
 
