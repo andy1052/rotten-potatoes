ab13743ea963eff41644b98fee306603a682bcb6
andy1052 <andy.ducharme@gmail.com>
2018-10-27 22:42:46 -0600

    Added Mongoose and Review model

--- app.js
+++ app.js
@@ -9,6 +9,17 @@
 //	Dependencies:
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/rotten-potatoes', {useNewUrlParser: true}, () => {
+	console.log('connected to database');
+});
+
+//	Mock mongoose model:
+const Review = mongoose.model('Review', {
+	title: String,
+	movieTitle: String
+});
 
 
 
@@ -23,20 +34,24 @@ app.set('view engine', 'handlebars');
 
 //	Make inital route:
 app.get('/', (req, res) => {
-	res.render('reviews-index', { reviews: reviews });
-});
+	//	Get reviews from database:
+	Review.find()
+		.then(reviews => {
+			res.render('reviews-index', { reviews: reviews });
+		})
+		.catch(err => {
+			console.log(err);
+		});
+	});
 
 
-/* A mock array of projects*/
-let reviews = [
-{ title: "Great Review", movieTitle: "Batman II"},
-{ title: "Awesome Review", movieTitle: "Titanic"}
-]
+// /* A mock array of projects*/
+// let reviews = [
+// { title: "Great Review", movieTitle: "Batman II"},
+// { title: "Awesome Review", movieTitle: "Titanic"},
+// { title: "Fuck Yeah!", movieTitle: "Fear and loathing in Las Vegas"}
+// ];
 
-/* Index Route Mock */
-app.get('/reviews', (req, res) => {
-
-});
 
 
 
