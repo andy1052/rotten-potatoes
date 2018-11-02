e902d68a4e0c82381f39c551e70d0d58fad5775a
andy1052 <andy.ducharme@gmail.com>
2018-11-02 06:43:49 -0600

    Users are now prohibited from certain resources without being logged in

--- controllers/comments.js
+++ controllers/comments.js
@@ -11,17 +11,24 @@ const Comment = require('../models/comment');
 module.exports = function(app) {
 
 	//	New Comments
-	app.post('/reviews/comments', (req, res) => {	
+	app.post('/reviews/comments', (req, res) => {
+	//	First check to make sure user is logged in:
+	if (req.user) {	
 		Comment.create(req.body).then(comment => {
 			res.redirect(`/reviews/${comment.reviewId}`);
 		}).catch((err) => {
 			console.log('Error from comments.js ', err.message);
 		});
+	} else {
+		res.redirect('/unauthorized');
+	}
 	});
 
 
 	//	Delete Comment:
 	app.delete('/reviews/comments/:id', (req, res) => {
+		//	First check to make sure the user is logged in:
+		if (req.user) {
 		console.log("Deleted Comment");
 		Comment.findByIdAndDelete(req.params.id).then((comment) => {
 	console.log(`${comment.reviewId}`);		
@@ -29,5 +36,8 @@ module.exports = function(app) {
 		}).catch((err) => {
 			console.log(err.message);
 		});
+	} else {
+		res.redirect('/unauthorized');
+	}
 	});
 };
--- controllers/reviews.js
+++ controllers/reviews.js
@@ -38,13 +38,23 @@ app.get('/reviews/new', (req, res) => {
 
 //	Create Route (Form Submit):
 app.post('/reviews', (req, res) => {
-	//	Save Review to database:
-	Review.create(req.body).then((review) => {
-		console.log(review);
-		res.redirect(`/reviews/${review._id}`); // redirect to reviews/:id
-	}).catch((err) => {
-		console.log(err.message);
+	//	Make sure user is logged in before saving new review:
+	if (req.user) {
+		//	Save Review to database:
+		Review.create(req.body).then((review) => {
+			console.log(review);
+			res.redirect(`/reviews/${review._id}`); // redirect to reviews/:id
+		}).catch((err) => {
+			console.log(err.message);
+		});
+			} else {
+				res.redirect('/unauthorized'); //Unauthorized.
+		}
 	});
+
+//	Get route for unauthorized:
+app.get('/unauthorized', (req, res) => {
+	res.render('unauthorized', {});
 });
 
 
@@ -78,23 +88,38 @@ app.get('/reviews/:id/edit', (req, res) => {
 
 //	Put request (Update), using "method-override" middleware:
 app.put('/reviews/:id', (req, res) => {
+	//	First check to make sure user is logged in.
+	if (req.user) {
 	Review.findOneAndUpdate(req.params.id, req.body)
 		.then(review => {
 			res.redirect(`/reviews/${review._id}`);
 		}).catch(err => {
 			console.log(err.message);
 		});
+	} else {
+		res.redirect('/unauthorized', {});
+	}
 });
 
 
 //	Delete Route:
 app.delete('/reviews/:id', (req, res) => {
+	//	First check to make sure user is logged in:
+	if (req.user) {
 	console.log("Deleted review");
 	Review.findOneAndDelete(req.params.id).then(review => {
 		res.redirect('/');
 	}).catch(err => {
 		console.log(err.message);
 	});
+} else {
+	res.redirect('/unauthorized');
+}
 });
+
+
+
+
+
 };
 
